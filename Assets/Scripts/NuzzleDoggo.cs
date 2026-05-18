73ea6fb172f5dcf7c649b9604e81666aafcec4d8
skytl <35516031+skytl@users.noreply.github.com>
2026-05-17 20:07:00 -0700

    Setup trigger volume to swap doggo to happy version, added shadows for cat's hidey to make it more obvious it's a tunnel, replaced small cactus sprite, and hooked background up to folllow the camera

--- Assets/Scripts/NuzzleDoggo.cs
+++ Assets/Scripts/NuzzleDoggo.cs
@@ -5,11 +5,39 @@ using UnityEngine;
 public class NuzzleDoggo : MonoBehaviour
 {
     public Collider2D dogCol;
+    public SpriteRenderer srCringe;
+    public SpriteRenderer srHappy;
+    public CapsuleCollider2D capsule;
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // verify collision object has the Player tag;
-        // set the doggo's anim to be the happy version instead!;
-        Debug.Log("TO DO: change doggo to be happy, after being nuzzled by cat");
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Testing?");
+            srCringe.enabled = false;
+            srHappy.enabled = true;
+        }
+    }
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            // disable collision on doggo's head, so player can jump straight up if they want to
+            capsule.enabled = false;
+        }
+    }
+
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            // reenable collision on doggo's head
+            capsule.enabled = true;
+        }
     }
 }
