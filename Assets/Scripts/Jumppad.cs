8d2c1cb326ae8831bd6c232da9de8a978a0294a7
skytl <35516031+skytl@users.noreply.github.com>
2026-05-25 22:52:53 -0700

    Reduced cushion velocity to 3, as a temporary measure to mitigate velocity getting out of hand. It's more fun at 2, though, so I need to find a better fix at some point

--- Assets/Scripts/Jumppad.cs
+++ Assets/Scripts/Jumppad.cs
@@ -3,21 +3,22 @@ using UnityEngine;
 public class Jumppad : MonoBehaviour
 {
     public float bounce = 20f;
-    public float cushionBounce = 10f;
+    //public float cushionBounce = 10f;
+    public int cushionVelocityReduction = 2;
     public Rigidbody2D jumppadRb;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // TO DO: Add a way to reset the player and the cushion's velocity, so it's not getting multiplied each time you jump off another one
         // TO DO: Figure out how to set this up so things like Fish, and Wineglasses can also be bounced off cushions
         // TO DO: Add a check to verify the cushion isGrounded, before anything can bounce off it. If it's not grounded, it should get knocked away, without bouncing things off it
         // TO DO: Add a check to make sure the thing bouncing off of it is either above or below (you shouldn't really bounce off the side of a cushion)
         if (collision.gameObject.CompareTag("Player"))
         {
-            Vector2 collisionVelocity = collision.relativeVelocity/2;
+            Vector2 collisionVelocity = collision.relativeVelocity/cushionVelocityReduction;
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
-            jumppadRb.AddForce(Vector2.up * cushionBounce, ForceMode2D.Impulse);
+            //jumppadRb.AddForce(Vector2.up * cushionBounce, ForceMode2D.Impulse);
             jumppadRb.AddForce(Vector2.one * collisionVelocity *-1, ForceMode2D.Impulse);
         }
     }
-
 }
